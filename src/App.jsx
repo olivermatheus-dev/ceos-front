64f2bd10f47f19512bcb7700de9e4ab3923bb74b
olivermnoo <riseumpro@gmail.com>
2023-02-11 13:08:47 -0300

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;

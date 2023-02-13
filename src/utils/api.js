282265953d0e5892a1f6b7c7a7af0b020b2d64ef
olivermnoo <riseumpro@gmail.com>
2023-02-13 17:11:40 -0300

    Update api.js
    Funcionando (assim esperamos)

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,12 +8,12 @@ import axios from "axios";
 //apiURL[process.env.NODE_ENV]
 
 const api = axios.create({
-  baseURL: "https://alestauranteapi.onrender.com/api",
+  baseURL: "https://ceosapi.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
   config.headers = {
-    Authorization: `Bearer 5ea9718ce2fc4d74d4a12fd6870ee6c8b92b7907d594d19df822c2da16137f4cf00c186cd3e413c0fd2f366576351e6699463574ffbed47b18cf309bd9b5fb6055aaa547eaafd603ff2c22dca9db49e6d8679ebf9b8ec6c9e57ed3c9535cf0acc09e7ca7f4e54def66eca7e735325790412fe66348efcc5dab86c4f6631ccfe3`,
+    Authorization: `Bearer 99fed76d1ffeb2dc640b7c3b2b5a91cf074920f0e86f5faa44cc1fc279e0fefbf8b2d8447cd049fc45e5f870d2b211cb04ba8c052ca6646bccb8670d89f459485f06a403cfc17e16120ab846c561b484a21cdfe601790b8f83e7f52ae1aae287cf130f4a577319c0617d6f1f98f635366b23cd7691fbe50e39653a7850ce47d1`,
   };
 
   return config;

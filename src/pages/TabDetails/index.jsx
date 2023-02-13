94610fbb3752cc0f2e0daa6b2311637e249c6164
olivermnoo <riseumpro@gmail.com>
2023-02-13 19:33:31 -0300

    Update model TabDetails

--- src/pages/TabDetails/index.jsx
+++ src/pages/TabDetails/index.jsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { api } from "../../utils/api";
 
 export function TabDetails() {
   const params = useParams();
@@ -18,9 +20,54 @@ export function TabDetails() {
   }, []);
 
   const tab = tabs.find((tabs) => {
-    tabs.id === Number(params);
+    return tabs.id === Number(params.tabId);
   });
   console.log(tab);
 
-  return <></>;
+  return (
+    <>
+      <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
+        <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
+          <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+            <header className="mb-4 lg:mb-6 not-format">
+              <address className="flex items-center mb-6 not-italic">
+                <div className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                  <img
+                    className="mr-4 w-16 h-16 rounded-full"
+                    src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
+                    alt="Jese Leos"
+                  />
+                  <div>
+                    <a
+                      href="#"
+                      rel="author"
+                      className="text-xl font-bold text-gray-900 dark:text-white"
+                    >
+                      {/* {tab.attributes.author} */}
+                    </a>
+                    <p className="text-base font-light text-gray-500 dark:text-gray-400">
+                      Graphic Designer, educator & CEO Flowbite
+                    </p>
+                    <p className="text-base font-light text-gray-500 dark:text-gray-400">
+                      <time dateTime="2022-02-08" title="February 8th, 2022">
+                        Feb. 8, 2022
+                      </time>
+                    </p>
+                  </div>
+                </div>
+              </address>
+              <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
+                Best practices for successful prototypes
+              </h1>
+            </header>
+            <p className="lead">
+              Flowbite is an open-source library of UI components built with the
+              utility-first classes from Tailwind CSS. It also includes
+              interactive elements such as dropdowns, modals, datepickers.
+            </p>
+          </article>
+        </div>
+      </main>
+    </>
+  );
 }

300e5adf63b4648aceb10b4493804c5a0d14eb4c
Danilo Shinzato <daniloshinzato@gmail.com>
2023-02-13 19:30:11 -0300

    Updatetab feito

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { CreateTab } from "./pages/Createtab";
 
 import { Home } from "./pages/Home";
 import { TabDetails } from "./pages/TabDetails";
+import { UpdateTab } from "./pages/Updatetab";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreateTab />} />
         <Route path="/tabdetails/:tabId" element={<TabDetails />} />
+        <Route path="/updatetab/:tabId" element={<UpdateTab />} />
       </Routes>
     </div>
   );
